53d629bb1562fdbd6fb8d83fd54664b3aba547e9
Sina Baniasad Azad <sina.baniasadazad@gmail.com>
2021-05-01 19:40:54 +0430

    Black and white checkered image

--- matrix/Checkeboard.m
+++ matrix/Checkeboard.m
@@ -7,9 +7,9 @@ clc
 %create the matrix
 a = randi([0 127], 256, 256);
 %converts the matrix into image
-K = mat2gray(a);
-min_image = min(K(:))
-max_image = max(K(:))
+k = mat2gray(a);
+min_image = min(k(:))
+max_image = max(k(:))
 %Image display
 figure
 imshow(K)
